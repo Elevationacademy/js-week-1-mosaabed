124d0ba04f06fb34c1ea3ba1a75a810bbc7f2c1b
mosa.abedelhady@mail.huji.ac.il <51644794+mosaabed@users.noreply.github.com>
2020-10-29 11:18:42 +0200

    Scope

--- main.js
+++ main.js
@@ -1,65 +1,111 @@
-// Objects
+// Scops
 
-//ex1
-let person1 = {
-    name:"Mosa",
-    age:22 ,
-    city: "Iksal" 
+//===================================================================================
+//Section 1 : error
+//===================================================================================
+const run = true  // run :global
+
+if (run) {
+    let distance = 8 // distance : local belong to the if scope
+    for (var i = 0; i < distance; i++) {
+        console.log("running")
+    }
+    console.log("Finished running " + distance + " miles")
 }
-let person2 = {
-    name : "Easa",
-    age :12 ,
-    city : "Iksal"
+
+
+console.log("Damn, you see this gal? She ran " + distance + " miles")    
+
+
+//===================================================================================
+//Section 2 : error
+//===================================================================================
+if (13 == "space") {
+  let cowSound = "moo"
 }
-if (person1.age == person2.age)
-{
-    if (person2.city == person1.city)
-    {
-        console.log("Jill wanted to date Robert")
-    }else{
-        console.log("Jill wanted to date Robert, but couldn't")
+else {
+  console.log("Cow says " + cowSound)
+}
+
+
+//===================================================================================
+//Section 3 : console will log:
+//            Served a special fish
+//            Served a special lettuce plate
+//            Served a special curious cheese
+//            Finished serving all the orders: fish,lettuce plate,curious cheese
+//===================================================================================
+const serveOrders = function (orders) {
+  // orders :local (serveOrders scope)
+  for (let order of orders) {
+      let specialOrder = "special " + order //orde & specialOrdeer : local (for scope)
+      console.log("Served a " + specialOrder)
+  }
+
+  console.log("Finished serving all the orders: " + orders)
+}
+const allOrders = ["fish", "lettuce plate", "curious cheese"] //global
+serveOrders(allOrders)
+
+
+
+//===================================================================================
+//Section 4 :error
+//===================================================================================
+const pot = "red pot with earth in it" //global
+
+const getSeed = function () {
+    const seed = "brown seed" // local (getseed scope)
+}
+
+const plant = function () {
+    getSeed()
+    console.log("Planting the " + seed + " inside a " + pot)
+}
+
+plant()
+
+
+
+//===================================================================================
+//Section 5 - Optional(if you have time) :error
+//===================================================================================
+const doesUserExist = function (name) {
+  const users = [{ name: "Shapira", age: 19 }, { name: "Lucius", age: 23 }] // local
+  for (let u of users) {
+      if (u.name == name) {
+          const found = true // local (for scope)
+      }
+  }
+  return found // errror
+}
+
+const userExists = doesUserExist("Lucius")
+if (userExists) {
+  console.log("We found the ragamuffin!")
+}
+else {
+  console.log("No idea where this person is.")
+}
+
+
+//===================================================================================
+//Section 6 - Optional (if you have time) :error
+//===================================================================================
+const isEnough = false //global
+
+const makeEnough = function () {
+    for (let i = 0; i < 10; i++) {
+        if (i > 5) {
+            isEnough = true // const can not be change error !!
+        }
     }
 }
 
-
-//ex2
-let myList =[{name :"Mosa" ,age : 21 },{name :"person", age : 80}]
-myList[0].age++
-myList.splice(1,1)
-console.log(myList)
-
-
-//ex3
-const lst1= [{name: "Younes", age: 25}, {name: "Easa", age: 12}]
-myList.push(...lst1)
-
-
-//ex4
-const library = {
-    books: [
-      {title: "tareek elnaja7", author: "Mosa"},
-      {title: "alsabr mofta7 elnja7", author: "Mosa"},
-    ]
-  }
-  myList.push(...library.books)
-
- 
-  
-// ex5 + ex5.1 + ex5.2
-const reservations = {
-  Bob: { claimed: false },
-  Ted: { claimed: true }
+makeEnough()
+if (isEnough) {
+    console.log("Finally, sheesh")
 }
-const editedName = lowerCaseName.charAt(0).toUpperCase() +
-                   lowerCaseName.slice(1)
-
-const name = prompt('Please enter the name for your reservation');
-if(reservations[name] && !reservations[name].claimed) {
-    alert("welcome" +name)
-  } else if (reservations[name] && reservations[name].claimed){
-    alert("someone already claimed this reservation")
-  } else if (!reservations[name]) {
-    alert("You have no reservation")
-    reservations[name] = {claimed: true}
-  }	
-    console.log(reservations)
+else {
+    console.log("Here we go again...")
+}
